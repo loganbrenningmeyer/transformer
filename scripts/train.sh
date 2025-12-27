94cf357886e81eb4a0bffa1a39a6add8c5ee60f9
Logan Brenningmeyer <loganbrenningmeyer@gmail.com>
2025-12-27 14:30:00 -0600

    began setting up TransformerSeq2Seq for training

--- scripts/train.sh
+++
@@ -1,4 +0,0 @@
-set -ex
-
-CONFIG=$1
-python -m transformer.train --config "$CONFIG"
--- src/transformer/data/datasets.py
+++ src/transformer/data/datasets.py
@@ -1,58 +1,89 @@
 import os
 import torch
+from torch.utils.data import Dataset
 import pandas as pd
+import datasets
+from omegaconf import DictConfig
 
-from transformer.utils.vocab import BPETokenizer
+from transformer.utils.tokenizer import BPETokenizer
 
 
-# class TokenDataset:
-#     def __init__(self, bpe: BPETokenizer, data_path: str, block_size: int=256, batch_size: int=8):
-#         self.block_size = block_size
-#         self.batch_size = batch_size
+class Seq2SeqDataset(Dataset):
+    def __init__(self, bpe: BPETokenizer, data_config: DictConfig):
+        self.block_size = data_config.block_size
+        self.batch_size = data_config.batch_size
+        self.source_key = data_config.source_key
+        self.target_key = data_config.target_key
 
-#         # ----------
-#         # Build vocab
-#         # ----------
-#         base_ids = bpe.load_data(data_path)
-#         bpe.build_vocab(base_ids)
+        # ----------
+        # Load dataset
+        # ----------
+        if data_config.dataset == "ted_talks":
+            dataset = datasets.load_dataset(
+                "IWSLT/ted_talks_iwslt", 
+                language_pair=("en","fr"),
+                year="2014"
+            )
 
-#         # ----------
-#         # Tokenize Text
-#         # ----------
-#         self.ids = torch.tensor(bpe.tokenize(base_ids), dtype=torch.long)
+            self.train_data = dataset["train"]
+            self.source_texts = []
+            self.target_texts = []
 
-#     def get_batch(self):
-#         """
-        
-        
-#         Args:
-        
-        
-#         Returns:
-        
-#         """
-#         indices = torch.randint(len(self.ids) - self.block_size, (self.batch_size,))
+            for sample in self.train_data["translation"]:
+                self.source_texts.append(sample[self.source_key])
+                self.target_texts.append(sample[self.target_key])
 
-#         source  = torch.stack([self.ids[i : i+self.block_size] for i in indices])
-#         target = torch.stack([self.ids[i+1 : i+self.block_size+1] for i in indices])
+        # ----------
+        # Build vocabulary
+        # ----------
+        all_texts = self.source_texts + self.target_texts
+        bpe.build_vocab(all_texts)
 
-#         return source, target
+        # ----------
+        # Tokenize text
+        # ----------
+        base_source_ids = [bpe.encode_text(text) for text in self.source_texts]
+        base_target_ids = [bpe.encode_text(text) for text in self.target_texts]
+
+        self.source_ids = [bpe.tokenize(ids) for ids in base_source_ids]
+        self.target_ids = [bpe.tokenize(ids) for ids in base_target_ids]
+
+    def __len__(self):
+        return len(self.train_data)
+
+    def __getitem__(self, idx: int):
+        """
+
+        """
+        source = torch.tensor(self.source_ids[idx], dtype=torch.long)
+        target = torch.tensor(self.target_ids[idx], dtype=torch.long)
+
+        return source, target
 
 
 class LMDataset:
-    def __init__(self, bpe: BPETokenizer, data_path: str, block_size: int=256, batch_size: int=8):
-        self.block_size = block_size
-        self.batch_size = batch_size
+    def __init__(self, bpe: BPETokenizer, data_config: DictConfig):
+        self.block_size = data_config.block_size
+        self.batch_size = data_config.batch_size
 
         # ----------
-        # Build vocab
+        # Load dataset
         # ----------
-        base_ids = bpe.load_data(data_path)
-        bpe.build_vocab(base_ids)
+        if data_config.dataset == "tiny_shakespeare":
+            dataset = datasets.load_dataset("karpathy/tiny_shakespeare")
+            self.train_text = dataset["train"]["text"][0]
+            self.valid_text = dataset["validation"]["text"][0]
+            self.test_text = dataset["test"]["text"][0]
 
         # ----------
-        # Tokenize Text
+        # Build vocabulary
         # ----------
+        bpe.build_vocab([self.train_text])
+
+        # ----------
+        # Tokenize text
+        # ----------
+        base_ids = bpe.encode_text(self.train_text)
         self.ids = torch.tensor(bpe.tokenize(base_ids), dtype=torch.long)
 
     def get_batch(self):
